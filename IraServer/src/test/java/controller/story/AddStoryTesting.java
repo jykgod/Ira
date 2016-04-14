d57bf7c72804c09c1a135c4662813b348d225e2d
xloypaypa <245532675@qq.com>
2016-04-14 17:26:49 +0800

    add more testing for adding story. and fixed db config

--- IraServer/src/test/java/controller/story/AddStoryTesting.java
+++ IraServer/src/test/java/controller/story/AddStoryTesting.java
@@ -2,9 +2,13 @@ package controller.story;
 
 import controller.LogicTesting;
 import controller.NoSendEvent;
+import model.db.StoryCollection;
 import model.entity.Priority;
+import model.entity.Story;
 import model.entity.StoryType;
+import net.sf.json.JSONObject;
 import org.junit.Test;
+import service.net.SessionManager;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,5 +23,27 @@ public class AddStoryTesting extends LogicTesting {
         NoSendEvent noSendEvent = addStory("1", "1", "1", StoryType.STORY, "", "", Priority.MINOR, 1);
 
         assertEquals("addStory", noSendEvent.getMessage().get(0).getKey());
+    }
+
+    @Test
+    public void should_fail_when_add_story_with_out_login() throws Exception {
+        JSONObject jsonObject = JSONObject.fromObject(new String(
+                addStory("1", "1", "1", StoryType.STORY, "", "", Priority.MINOR, 1)
+                        .getMessage().get(0).getValue()));
+
+        assertEquals("fail", jsonObject.getString("result"));
+    }
+
+    @Test
+    public void should_be_the_watcher_when_add_ok() throws Exception {
+        loginApp("test user");
+        JSONObject jsonObject = JSONObject.fromObject(new String(
+                addStory("1", "1", "1", StoryType.STORY, "", "", Priority.MINOR, 1)
+                        .getMessage().get(0).getValue()));
+        Story story = new StoryCollection().getStoryData("1");
+
+        assertEquals("ok", jsonObject.getString("result"));
+        assertEquals(1, story.getWatcher().size());
+        assertEquals("test user", story.getWatcher().get(0));
     }
 }
